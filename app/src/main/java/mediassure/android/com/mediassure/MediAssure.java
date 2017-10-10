715a42264a5e7ff266da0d893d5df36a17784090
hathorr-hub <soumendebnath.cs@gmail.com>
2017-10-10 21:55:30 +0530

    Initial commit

--- app/src/main/java/mediassure/android/com/mediassure/MediAssure.java
+++ app/src/main/java/mediassure/android/com/mediassure/MediAssure.java
@@ -1,7 +1,8 @@
 package mediassure.android.com.mediassure;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MediAssure extends AppCompatActivity {
 
